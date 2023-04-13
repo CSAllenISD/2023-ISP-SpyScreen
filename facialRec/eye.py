133a657896995df87a7ac20f1357d31e27a59700
Drew-Eldridge <dreweldridge44@gmail.com>
2023-04-12 23:32:18 -0500

    added eye tracking files (doesnt work yet)

--- facialRec/eye.py
+++ facialRec/eye.py
@@ -1,4 +1,3 @@
-# pip install -r requirements.txt
 import threading
 import cv2
 import os
@@ -14,15 +13,14 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 counter = 0
 face_match = False
-file = "faces/drew.jpg"
+file = "drew.jpg"
 reference_img = cv2.imread(file)
 regex_pattern = '[\w-]+?(?=\.)'
 result = re.search(regex_pattern, file)
 name = result.group().capitalize()
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-# faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
 def check_face(frame):
     global face_match
@@ -39,6 +37,8 @@ while True:
     
     if ret: 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # convert to grayscale
+        
+        # detect faces
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=100)
         if counter % 30 == 0:
             try:
@@ -47,28 +47,29 @@ while True:
                 pass
         counter += 1
         
-        # if face_match:
-        #     cv2.putText(frame, "Drew", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
-        # else:
-        #     cv2.putText(frame, "UNKNOWN", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-
-        # detect faces
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # convert BGR to RGB
-        faces = functions.extract_faces(rgb_frame.copy(), enforce_detection=False)
-
+        # detect eyes
         if len(faces) > 0:
-            face = faces[0]
-            box = face[1]
-            (x, y, w, h) = box['x'], box['y'], box['w'], box['h']
-            if face_match:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                cv2.putText(frame, name, (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-            else:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                cv2.putText(frame, "UNKNOWN", (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-                    
-        cv2.imshow("video", frame)
-        
+            for (x, y, w, h) in faces:
+                roi_gray = gray[y:y + h, x:x + w]
+                roi_color = frame[y:y + h, x:x + w]
+                eyes = eye_cascade.detectMultiScale(roi_gray)
+                for (ex, ey, ew, eh) in eyes:
+                    cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+            # display result
+            face = faces[0] if len(faces) > 0 else None
+            if face is not None:
+                box = face
+                (x, y, w, h) = box
+                if face_match:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    cv2.putText(frame, name, (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+                else:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                    cv2.putText(frame, "UNKNOWN", (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+
+            cv2.imshow("video", frame)
+                
     key = cv2.waitKey(1)
     if key == 27:
         break
