d234d7fd1a1382ccc498e8cb50ba463cb044478e
Drew-Eldridge <dreweldridge44@gmail.com>
2023-05-06 12:02:04 -0500

    added test file update

--- facialRec/test.py
+++ facialRec/test.py
@@ -1,70 +1,30 @@
-import threading
 import cv2
-import os
-import re
-import numpy as np
-from deepface import DeepFace
-from deepface.commons import functions
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+# Create a VideoCapture object to access the default camera
+cap = cv2.VideoCapture(0)
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# Check if the camera is opened successfully
+if not cap.isOpened():
+    print("Error opening the camera")
+    exit()
 
-counter = 0
-face_match = False
-file = "faces/drew.jpg"
-reference_img = cv2.imread(file)
-regex_pattern = '[\w-]+?(?=\.)'
-result = re.search(regex_pattern, file)
-name = result.group().capitalize()
-
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-def check_face(frame):
-    global face_match
-    try:
-        if DeepFace.verify(frame, reference_img.copy())['verified']:
-            face_match = True
-        else:
-            face_match = False
-    except ValueError:
-        face_match = False
-
-def draw_faces(frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # convert to grayscale
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=100)
-
-    # detect faces
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # convert BGR to RGB
-    faces = functions.extract_faces(rgb_frame.copy(), enforce_detection=False)
-
-    if len(faces) > 0:
-        face = faces[0]
-        box = face[1]
-        (x, y, w, h) = box['x'], box['y'], box['w'], box['h']
-        if face_match:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(frame, name, (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-        else:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            cv2.putText(frame, "UNKNOWN", (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-
-    cv2.imshow("Face Recognition", frame)
-
+# Keep capturing frames from the camera until user presses 'q' key
 while True:
+    # Read a frame from the camera
     ret, frame = cap.read()
 
-    if ret:
-        if counter % 30 == 0:
-            threading.Thread(target=check_face, args=(frame.copy(),)).start()
-        counter += 1
-
-        threading.Thread(target=draw_faces, args=(frame.copy(),)).start()
-
-    key = cv2.waitKey(1)
-    if key == 27:
+    # Check if the frame was successfully captured
+    if not ret:
+        print("Error capturing the frame")
         break
 
-cv2.destroyAllWindows()
+    # Display the frame in a window named "Webcam"
+    cv2.imshow("Webcam", frame)
+
+    # Wait for user to press 'q' key to exit
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Release the camera and close all windows
 cap.release()
+cv2.destroyAllWindows()
